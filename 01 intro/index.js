da936554406f96be29f564bbfd86fa584814121e
Rahul Sharma <rahul@example.com>
2026-09-14 21:06:22 +0530

    intro assignment

---
+++ 01 intro/index.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('myBtn');
+ button.addEventListener('click', function() 
+        
+ alert("Hello, B.Tech Student!");
+
+          
+console.log("JavaScript is running successfully!");
+        
